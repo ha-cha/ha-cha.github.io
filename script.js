eece0c9738e2d06023da0d86587393cdcf92e3f2
kachoocha <135176450+ha-cha@users.noreply.github.com>
2023-05-31 21:31:13 -0500

    init

---
+++ script.js
@@ -0,0 +1,13 @@
+
+    var element,
+element = document.getElementById('themeSwitch');
+if (element != null) {
+    click = element.value;
+}
+else {
+    str = null;
+    console.log("good test");
+    element1223 .addEventListener("click", function() {
+
+    });
+}
